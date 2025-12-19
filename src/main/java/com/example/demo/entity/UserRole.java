244aa55dd57db93b0dbe15205d8b6e7b0775ecd1
bala-amypo <bala@amypo.in>
2025-12-19 03:59:15 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserRole.java
+++ src/main/java/com/example/demo/entity/UserRole.java
@@ -1,18 +1,26 @@
 package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_roles")
 public class UserRole {
-private Long id;
-private Long userId;
-private Long roleId;
-public UserRole() {}
-public UserRole(Long id, Long userId, Long roleId) {
-this.id = id;
-this.userId = userId;
-this.roleId = roleId;
-}
-public Long getId() { return id; }
-public void setId(Long id) { this.id = id; }
-public Long getUserId() { return userId; }
-public void setUserId(Long userId) { this.userId = userId; }
-public Long getRoleId() { return roleId; }
-public void setRoleId(Long roleId) { this.roleId = roleId; }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private Long roleId;
+
+    public UserRole() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public Long getRoleId() { return roleId; }
+    public void setRoleId(Long roleId) { this.roleId = roleId; }
 }
