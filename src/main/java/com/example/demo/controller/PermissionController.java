6d80232669d2e7d2a885e6f2e5d2fab01e448d13
bala-amypo <bala@amypo.in>
2025-12-19 04:14:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/PermissionController.java
+++ src/main/java/com/example/demo/controller/PermissionController.java
@@ -3,6 +3,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.Permission;
 import com.example.demo.service.PermissionService;
+@RestController
+@RequestMapping("/api/permissions")
+public class PermissionController {
+@Autowired
+PermissionService service;
+
+@PostMapping("/add")
+public Permission add(@RequestBody Permission permission) {
+return service.addPermission(permission);
+}
+@GetMapping("/get/{id}")
+public Permission get(@PathVariable Long id) {
+return service.getPermission(id);
+}
+}
 
 @RestController
 @RequestMapping("/api/permissions")
