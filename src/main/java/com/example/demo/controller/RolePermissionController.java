d392167ae0cb3908d51601683f06a06237831396
bala-amypo <bala@amypo.in>
2025-12-19 06:28:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/RolePermissionController.java
+++ src/main/java/com/example/demo/controller/RolePermissionController.java
@@ -1,19 +1,19 @@
-// package com.example.demo.controller;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
-// import com.example.demo.entity.RolePermission;
-// import com.example.demo.service.RolePermissionService;
-// @RestController
-// @RequestMapping("/api/role-permissions")
-// public class RolePermissionController {
-// @Autowired
-// RolePermissionService service;
-// @PostMapping("/add")
-// public RolePermission add(@RequestBody RolePermission rp) {
-// return service.grantPermission(rp);
-// }
-// @GetMapping("/get/{id}")
-// public RolePermission get(@PathVariable Long id) {
-// return service.getMapping(id);
-// }
-// }
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.RolePermission;
+import com.example.demo.service.RolePermissionService;
+@RestController
+@RequestMapping("/api/role-permissions")
+public class RolePermissionController {
+@Autowired
+RolePermissionService service;
+@PostMapping("/add")
+public RolePermission add(@RequestBody RolePermission rp) {
+return service.grantPermission(rp);
+}
+@GetMapping("/get/{id}")
+public RolePermission get(@PathVariable Long id) {
+return service.getMapping(id);
+}
+}
