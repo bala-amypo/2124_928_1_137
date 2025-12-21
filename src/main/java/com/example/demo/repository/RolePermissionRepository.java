3b61f3c4ca45986e7c3be23814ddf377476d52b9
bala-amypo <bala@amypo.in>
2025-12-21 13:39:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/RolePermissionRepository.java
+++ src/main/java/com/example/demo/repository/RolePermissionRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 import com.example.demo.entity.RolePermission;
 
-@Repository
-public interface RolePermissionRepository extends JpaRepository<RolePermission, Long> {}
+public interface RolePermissionRepository extends JpaRepository<RolePermission, Long> {
+    List<RolePermission> findByRole_Id(Long roleId);
+}
--- src/main/java/com/example/demo/repository/UserRoleRepository.java
+++ src/main/java/com/example/demo/repository/UserRoleRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 import com.example.demo.entity.UserRole;
 
-@Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {}
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    List<UserRole> findByUser_Id(Long userId);
+}
