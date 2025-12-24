faf9806d14e6975d777404a2b502042fa1c20f15
bala-amypo <bala@amypo.in>
2025-12-24 06:03:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRoleRepository.java
+++ src/main/java/com/example/demo/repository/UserRoleRepository.java
@@ -1,10 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.UserRole;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-
     List<UserRole> findByUser_Id(Long userId);
 }
